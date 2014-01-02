fb31015aeff761feae6b5bcaa87edcaf3217c78d
comettom <buscando.php@gmail.com>
2014-01-02 10:02:53 -0300

    Saque carpetas de mas

--- ANTERIOR/pruebas/pruebagoogle.php
+++
@@ -1,9 +0,0 @@
-<?
-include('/var/www/vhosts/marcelo/apdb1.no-ip.info/httpdocs/blogsviolentos/search_google_urlimg.php');
-
-$resultado=search_google_urimg("cordoba",20);
-print_r($resultado);
-?>
-
-
-
