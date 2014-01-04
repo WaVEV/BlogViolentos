bd372c1fbc116a0b70fb27583f23ba84744a4247
comettom <buscando.php@gmail.com>
2014-01-04 13:02:09 -0300

    Cambios violentos, Eric los propicio

--- colaboradores/eric/prueba.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_reporting(E_ERROR | E_PARSE);
-echo mb_detect_encoding("el camión de España");
-?>
--- colaboradores/eric/unit_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-//include ("../listos/sintaxis_nltk.php");
-include ("classTexto.php");
-//echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'; 
-
-print_r(BuscaDefinicion("museo"));
-
-
-?>
