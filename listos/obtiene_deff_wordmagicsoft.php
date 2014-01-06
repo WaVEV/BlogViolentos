2226a2b055bc6dfe222d8548ac55c908ca9521bc
WaVEV <emanuel.lupi91@gmail.com>
2014-01-06 10:27:22 -0300

    arreglado el obtiene sinonimos, tenia un problema de codificacion

--- listos/obtiene_deff_wordmagicsoft.php
+++ listos/obtiene_deff_wordmagicsoft.php
@@ -1,12 +1,15 @@
 <?php
 function TakeDeff($word)
 {
-    $html = file_get_contents("http://www.wordmagicsoft.com/diccionario/es-en/" . urldecode($word) . ".php");
+	$url = str_replace('+' , '%20' ,  urlencode($word));
+    $html = file_get_contents("http://www.wordmagicsoft.com/diccionario/es-en/" . $url . ".php");
     preg_match_all('/explanation\'[^>]*>[^<>]*Definir significado de[^<>]*<\/span>([^<>]*)/i', $html, $mat);
     return array_map('trim', $mat[1]);
 }
+
 function TakeSyn($word){
-    $html = file_get_contents("http://www.wordmagicsoft.com/diccionario/es-en/" . urldecode($word) . ".php");
+	$url = str_replace('+' , '%20' ,  urlencode($word));
+    $html = file_get_contents("http://www.wordmagicsoft.com/diccionario/es-en/" . $url . ".php");
     preg_match_all('/explanation\'[^>]*>[^<>]*Sin[^n]{1,4}nimos[^<>:]*: *(<\/span>)?([^<>]*)/i', preg_replace("/<\/?a[^>]*>/i", "", $html), $mat) ||
     preg_match_all('/Sin[^n]{1,4}nimos[^<>:]*: *(<\/span>)?([^<>;]*)/i', preg_replace("/<\/?a[^>]*>/i", "", $html), $mat);
     return array_map(function($a){return explode(",", $a);}, $mat[2]);
